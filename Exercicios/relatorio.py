ad26df10340c92938427b313d4299075ccbda137
higor costa <higorcosta2001@outlook.com>
2021-02-26 19:02:49 -0300

    Relatorio

---
+++ Exercicios/relatorio.py
@@ -0,0 +1,14 @@
+
+print('Oi, hoje o Python vai contar uma fábula para você...')
+print('')
+name = str(input('Qual o sua sugestão de nome para o personagem principal ? '))
+age = int(input('E a idade dele ? '))
+print('')
+print('Uma raposa(o) faminta(o) chamada(o) {} que tinha {} anos de idade, ao ver cachos de uva'.format(name,age))
+print('suspensos em uma parreira, quis pegá-los')
+print('mas não conseguiu. Então, afastou-se dela, dizendo: “Estão verdes”.')
+print('')
+print('Moral: Assim como a/o {}, alguns homens, não conseguindo realizar'.format(name))
+print('seus negócios por incapacidade e acabam criando desculpas para o fracasso.')
+print('')
+
