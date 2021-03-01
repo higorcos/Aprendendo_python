d99946b30eae28e1349a05f626fb45e0fc8d2d3a
higor costa <higorcosta2001@outlook.com>
2021-03-01 19:28:01 -0300

    Relatorio comentado

--- Exercicios/relatorio.py
+++ Exercicios/relatorio.py
@@ -1,14 +1,20 @@
-
-print('Oi, hoje o Python vai contar uma fábula para você...')
-print('')
+print('Oi, hoje o Python vai contar uma fábula para você...') #avisa que vai contar uma história
+print('')# cria um linha sem caracteres para organizar o código
+"""
+Nessa parte do código o computador vai fazer alguma perguntas para o usuário
+as respostas dessas perguntas serão usadas para completar a história.
+"""
 name = str(input('Qual o sua sugestão de nome para o personagem principal ? '))
-age = int(input('E a idade dele ? '))
+age = int(input('E a idade dele(a) ? '))
+fruit = str(input('Fale um nome de uma fruta ? '))
+"""
+Nessa parte o computador vai printar o texto no terminal e vai completar com as respostas do usuário.
+"""
 print('')
-print('Uma raposa(o) faminta(o) chamada(o) {} que tinha {} anos de idade, ao ver cachos de uva'.format(name,age))
-print('suspensos em uma parreira, quis pegá-los')
-print('mas não conseguiu. Então, afastou-se dela, dizendo: “Estão verdes”.')
-print('')
-print('Moral: Assim como a/o {}, alguns homens, não conseguindo realizar'.format(name))
-print('seus negócios por incapacidade e acabam criando desculpas para o fracasso.')
-print('')
-
+print('Uma raposa(o) faminta(o) chamada(o) {} que tinha {} anos de idade, ao ver uma {}'.format(name,age,fruit)) #as variáveis serão printadas no terminal nas ordem  (name,age,fruit)
+print('quis pegá-la(o) mas não conseguiu. Então, afastou-se da fruta, dizendo: “Não queria mesmo”.')
+print('') #linha em branco para organização
+print('Moral da História: ')
+print('Assim como a/o {}, algumas pessoas não conseguem alcançar'.format(name)) # a variável é colocada dentro dos {}
+print('seus objetivo e acabam criando desculpas para o fracasso.')
+print('') #linha em branco para organização
