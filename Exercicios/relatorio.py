cde1acce4fa26e5d42b6cc04dbc452c60bb89ed7
higor costa <higorcosta2001@outlook.com>
2021-03-09 11:36:34 -0300

    Comentário de uma linha

--- Exercicios/relatorio.py
+++ Exercicios/relatorio.py
@@ -1,15 +1,15 @@
 print('Oi, hoje o Python vai contar uma fábula para você...') #avisa que vai contar uma história
 print('')# cria um linha sem caracteres para organizar o código
-"""
-Nessa parte do código o computador vai fazer alguma perguntas para o usuário
-as respostas dessas perguntas serão usadas para completar a história.
-"""
+
+#Nessa parte do código o computador vai fazer alguma perguntas para o usuário
+#as respostas dessas perguntas serão usadas para completar a história.
+
 name = str(input('Qual o sua sugestão de nome para o personagem principal ? '))
 age = int(input('E a idade dele(a) ? '))
 fruit = str(input('Fale um nome de uma fruta ? '))
-"""
-Nessa parte o computador vai printar o texto no terminal e vai completar com as respostas do usuário.
-"""
+
+#Nessa parte o computador vai printar o texto no terminal e vai completar com as respostas do usuário.
+
 print('')
 print('Uma raposa(o) faminta(o) chamada(o) {} que tinha {} anos de idade, ao ver uma {}'.format(name,age,fruit)) #as variáveis serão printadas no terminal nas ordem  (name,age,fruit)
 print('quis pegá-la(o) mas não conseguiu. Então, afastou-se da fruta, dizendo: “Não queria mesmo”.')
