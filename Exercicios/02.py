0afd633f451d2232e52233e150e7d83d7fb727df
higor costa <higorcosta2001@outlook.com>
2021-03-10 16:33:38 -0300

    exercicios

---
+++ Exercicios/02.py
@@ -0,0 +1,7 @@
+var = 2
+var2 = var ** (1/2)
+
+print('Vidente \n') 
+print('Número {} \nO antecessor é {} o sucessor é {}'.format(var,(var-1),(var+1)), end="") #\n quebra de linha #end linga os dois prints
+print("")
+print('Rais quadrada de {} é {}\n'.format(var,var2))
---
+++ Exercicios/03.py
@@ -0,0 +1,5 @@
+var1 = float(input('Qual a sua primira nota? '));
+var2 = float(input('Qual a sua segunda nota? '));
+var3 = float(input('Qual a sua terceira nota? '));
+Media = float((var1+var2+var3)/3)
+print('A sua media na materia ficará {:.1f}'.format(Media));#:.1f número de casas após a virgula 
