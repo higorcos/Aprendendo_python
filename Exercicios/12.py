8e7f702d18cba8880e95610aebb3b4af7eda0cc1
higor costa <higorcosta2001@outlook.com>
2021-04-10 19:20:40 -0300

    Correção  graus

--- Exercicios/12.py
+++ Exercicios/12.py
@@ -2,17 +2,23 @@ import numpy as np
 from matplotlib.pyplot import subplots
 
 a = np.linspace(-2*np.pi,2*np.pi, 5)
-print(a)
+print('Radiano',a)
+#conversão
+graus = (a* 180/np.pi) #vai transformar o radianos em graus
+print('Graus',graus) # será aproximado 
 # cosseno
-y1 = np.cos(a)
+y1 = np.cos(graus)
 # seno
-y2 = np.sin(a)
+y2 = np.sin(graus)
+print('Seno',y2)
+print('Cosseno',y1)
+print('')
 fig, ax = subplots(1,2)
 # plot seno
-ax[0].plot(a, y2)
+ax[0].plot(graus, y2)
 ax[0].set_title("Funçao Seno")
 # plot cosseno
-ax[1].plot(a, y1)
+ax[1].plot(graus, y1)
 ax[1].set_title("Funçao Cosseno")
 
 
